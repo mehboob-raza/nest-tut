8aded65c9ad7764340fb8abafa4ae9b7187b06ad
unknown <razamehboob61@gmail.com>
2026-03-11 04:18:02 +0500

    DTOs and interfaces in customer

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -3,10 +3,11 @@ import { CreateCustomerDto } from './dto/create-customer.dto';
 import { Customer } from './interfaces/customer.interface';
 @Injectable()
 export class CustomerService {
-    private customers: Customer[] = []
+    private customers: Customer[] = [] 
 
     getAllCustomers() {
         return this.customers
+        
     }
 
     addCustomer(createCustomerDTO: CreateCustomerDto): Customer {
